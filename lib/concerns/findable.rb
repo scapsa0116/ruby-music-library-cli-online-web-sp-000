8ef72abfd41d51a0218664c5b90a706bd7f71753
Stela Capsa <stela.capsa@gmail.com>
2020-01-25 16:08:37 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -22,29 +22,29 @@
 
 
 
-module Concerns
-  module Findable
-  def find_by_name(name)
-    self.all.detect {|song| song.name == name}
-  end
-end 
+# module Concerns
+#   module Findable
+#   def find_by_name(name)
+#     self.all.detect {|song| song.name == name}
+#   end
+# end 
 
-  def find_or_create_by_name(name)
-    # if self.find_by_name(name)
-    #   self.find_by_name(name)
-    # else
-    # song = self.create(name)
-    # end
+#   def find_or_create_by_name(name)
+#     # if self.find_by_name(name)
+#     #   self.find_by_name(name)
+#     # else
+#     # song = self.create(name)
+#     # end
 
-    self.find_by_name(name) || self.create(name)
+#     self.find_by_name(name) || self.create(name)
 
-# def find_or_create_by_name(name)
-#   if self.find_by_name(name) == nil 
-#   self.create(name)
-#   else 
-#   self.find_by_name(name)
-#   end 
+# # def find_or_create_by_name(name)
+# #   if self.find_by_name(name) == nil 
+# #   self.create(name)
+# #   else 
+# #   self.find_by_name(name)
+# #   end 
 
-  end
-end
+#   end
+# end
 
