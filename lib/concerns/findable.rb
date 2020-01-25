b378bb836ec7d97550bd671e234e09a0ffb20eeb
Stela Capsa <stela.capsa@gmail.com>
2020-01-25 16:08:42 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -48,3 +48,21 @@
 #   end
 # end
 
+
+module Concerns::Findable
+  def find_by_name(name)
+    self.all.detect {|song| song.name == name}
+  end
+
+  def find_or_create_by_name(name)
+    # if self.find_by_name(name)
+    #   self.find_by_name(name)
+    # else
+    # song = self.create(name)
+    # end
+
+    self.find_by_name(name) || self.create(name)
+
+
+  end
+end
