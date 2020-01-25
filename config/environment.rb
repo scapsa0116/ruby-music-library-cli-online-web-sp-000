9bd2d16d4dab4414a37091f6278276b0f099985d
Stela Capsa <stela.capsa@gmail.com>
2020-01-25 16:10:05 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,28 +2,9 @@ require 'bundler'
 Bundler.require
 
 module Concerns
-#   module Findable 
-  
-  
-#   def find_by_name(name)
-#     all.map do |song|
-#       if song.name == name 
-#         return song 
-#       end 
-#     end 
-# end 
-# end 
-
-
-# def find_or_create_by_name(name)
-#   if self.find_by_name(name) == nil 
-#   self.create(name)
-#   else 
-#   self.find_by_name(name)
-#   end 
-# end
-
 module Findable
+  
+  
   def find_by_name(name)
     self.all.detect {|song| song.name == name}
   end
@@ -37,13 +18,6 @@ end
     # end
 
     self.find_by_name(name) || self.create(name)
-
-# def find_or_create_by_name(name)
-#   if self.find_by_name(name) == nil 
-#   self.create(name)
-#   else 
-#   self.find_by_name(name)
-#   end 
 
  end
 end 
