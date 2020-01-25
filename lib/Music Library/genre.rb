f844dcce28d443ebe8fd32a6f76fc404f59d8e82
Stela Capsa <stela.capsa@gmail.com>
2020-01-25 15:50:59 +0000

    Automatically backed up by Learn

--- lib/Music Library/genre.rb
+++ lib/Music Library/genre.rb
@@ -1,41 +1,41 @@
-class Genre 
-  extend Concerns::Findable 
-  attr_accessor :name, :songs, :artist 
+# class Genre 
+#   extend Concerns::Findable 
+#   attr_accessor :name, :songs, :artist 
   
- @@all = []
+# @@all = []
  
- def initialize(name)
-   @name = name 
-   @songs = []
-   save 
+# def initialize(name)
+#   @name = name 
+#   @songs = []
+#   save 
    
- end 
+# end 
  
- def self.all 
-    @@all 
-  end 
+# def self.all 
+#     @@all 
+#   end 
   
-  def save 
-    @@all << self 
-  end 
+#   def save 
+#     @@all << self 
+#   end 
   
-   def self.create(name)
-  genre =Genre.new(name)
-  self.all << genre 
-  genre
-end 
+#   def self.create(name)
+#   genre =Genre.new(name)
+#   self.all << genre 
+#   genre
+# end 
   
-  def self.destroy_all
-    @@all.clear
-  end 
+#   def self.destroy_all
+#     @@all.clear
+#   end 
   
  
  
- def artists
-    self.songs.collect{|song| song.artist}.uniq
-  end
+# def artists
+#     self.songs.collect{|song| song.artist}.uniq
+#   end
   
-  def to_s 
-    self.name 
-  end 
-end 
+#   def to_s 
+#     self.name 
+#   end 
+# end 
