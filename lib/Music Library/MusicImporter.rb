6143f2605652122e8dfff606c000a5a136b283b9
Stela Capsa <stela.capsa@gmail.com>
2020-01-25 15:51:38 +0000

    Automatically backed up by Learn

--- lib/Music Library/MusicImporter.rb
+++ lib/Music Library/MusicImporter.rb
@@ -17,3 +17,22 @@
 #     files.each{|file| Song.new_by_}
 #   end
 # end 
+
+
+class MusicImporter
+  attr_accessor :path, :files
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+      @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+  end
+
+  def import
+    files.each {|file| Song.create_from_filename(file) }
+  end
+
+
+end
